da04f629fea625f563efed4c5f114c12e438b13b
Anfo <longjiahui@hotmail.com>
2023-08-21 11:07:16 +0800

    build: add cjs mjs build script

--- build.js
+++ build.js
@@ -1,19 +1,31 @@
-import * as esbuild from 'esbuild'
+// import * as esbuild from 'esbuild'
+const esbuild = require('esbuild')
 
-await esbuild.build({
-    entryPoints: ['main.ts'],
-    bundle: true,
-    outfile: 'dist/main.mjs',
-    format: 'esm',
-    target: 'esnext',
-    platform: 'node',
-    banner: {
-        js: `
-    // BANNER START
-    const require = (await import("node:module")).createRequire(import.meta.url);
-    const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
-    const __dirname = (await import("node:path")).dirname(__filename);
-    // BANNER END
-    `,
-    },
-})
+;(async () => {
+    await esbuild.build({
+        entryPoints: ['main.ts'],
+        bundle: true,
+        outfile: 'dist/main.js',
+        format: 'esm',
+        target: 'esnext',
+        platform: 'node',
+        banner: {
+            js: `
+// BANNER START
+const require = (await import("node:module")).createRequire(import.meta.url);
+const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+const __dirname = (await import("node:path")).dirname(__filename);
+// BANNER END
+        `,
+        },
+    })
+
+    await esbuild.build({
+        entryPoints: ['main.ts'],
+        bundle: true,
+        outfile: 'dist/main.cjs',
+        format: 'cjs',
+        target: 'esnext',
+        platform: 'node',
+    })
+})()
